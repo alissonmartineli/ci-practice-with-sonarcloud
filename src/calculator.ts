2be50a39e82bbf86fa487d7e93874a7c7f731d12
Alisson Martineli <martineli.alisson@gmail.com>
2022-07-24 00:04:08 -0300

    feat: add diff, product and divide operations

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,6 +1,18 @@
 const mathOperations = {
     sum: (a: number, b: number): number => {
         return a + b
+    },
+
+    diff: (a: number, b: number): number => {
+        return a - b
+    },
+
+    product: (a: number, b: number): number => {
+        return a * b
+    },
+
+    divide: (a: number, b: number): number => {
+        return a / b
     }
 }
 
